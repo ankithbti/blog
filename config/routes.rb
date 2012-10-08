cd8f3d0a93ccd08fa0db64f0cc68cb9262e3900a
Ankit Gupta <ankithbti007@gmail.com>
2012-10-08 23:14:05 +0530

    Signup page complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blog::Application.routes.draw do
+  get "users/new"
+
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
@@ -7,6 +9,7 @@ Blog::Application.routes.draw do
   #match '/', to: 'static_pages#home'
   root to: 'static_pages#home'
 
+  match '/signup', to: 'users#new'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
