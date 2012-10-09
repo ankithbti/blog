14f83dbd7d615c031cfd637c34b9e22520b155f9
ruby <ruby@ankit-Aspire-5738.(none)>
2012-10-09 22:36:02 +0530

    Signup for blog application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '63d8ac7edcaceb4aacb6fb674750c5a41df6cb2b6d318308b99c915809e276c4509eeac596be06939405bb48ba32f8cdda637716969bff7c960de9a878cbcd9e'
